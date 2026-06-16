49215d9d0f0b1e141c2afb27124bd79b8cd4ee9a
Oscar Horacio Lopez Navarrete <lopznava@gmail.com>
2026-06-16 00:10:05 -0600

    Clase 8: Implementación de Factorial iterativo vs recursivo y resolución de desbordamiento mediante BigInteger

---
+++ Entregable8_Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics; // ¡Librería obligatoria para alta precisión!
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== PARTE B: REFACTORIZACIÓN PROFESIONAL CON BIGINTEGER ===\n");
+        
+        // En Main — prueba con n = 100
+        BigInteger resultado = FactorialProfesional(100);
+        
+        Console.WriteLine($"100! = {resultado}");
+    }
+
+    // --- Función Recursiva de Alta Precisión ---
+    static BigInteger FactorialProfesional(BigInteger n) 
+    {     
+        // Caso Base: Usamos BigInteger.One en lugar de 1 para evitar conversiones implícitas
+        if (n == 0 || n == 1)         
+            return BigInteger.One;     
+        
+        // Caso Recursivo
+        return n * FactorialProfesional(n - 1); 
+    }
+}
